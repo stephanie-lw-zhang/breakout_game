151db7baf25c3e18761f037128ec7bd09901435c
Matt Rose <mjr68@duke.edu>
2019-02-02 17:42:42 -0500

    updated bouncer

--- src/Bouncer.java
+++ src/Bouncer.java
@@ -1,59 +1,55 @@
-
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
-import javafx.application.Application;
 import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.KeyCode;
-import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
-import javafx.scene.shape.Shape;
-import javafx.stage.Stage;
-import javafx.util.Duration;
+
 
 public class Bouncer {
 
-    public static final int BOUNCER_SPEED = 30;
+    public static final int BOUNCER_SPEED = 60;
 
     private int myVelocityX = 1;
     private int myVelocityY = 1;
-    private int x, y;
+    private int numLives = 3;
     private ImageView myBouncer;
 
 
-    public Bouncer(ImageView myBouncer, int x, int y){
+    public Bouncer(ImageView myBouncer, double screenWidth, double screenHeight){
         this.myBouncer = myBouncer;
-        this.x = x;
-        this.y = y;
+        myBouncer.setX(screenWidth / 2 - myBouncer.getBoundsInLocal().getWidth() / 2);
+        myBouncer.setY(screenHeight / 2 - myBouncer.getBoundsInLocal().getHeight() / 2);
     }
 
     public ImageView getView(){
         return myBouncer;
     }
 
-    public void move(double elapsedTime) {
+    public void move(double elapsedTime, double screenHeight, double screenWidth) {
         myBouncer.setX(myBouncer.getX() - myVelocityX * BOUNCER_SPEED * elapsedTime);
-        myBouncer.setY(myBouncer.getY() - myVelocityY * BOUNCER_SPEED * elapsedTime);
-    }
-
-    public void bounce(double screenWidth, double screenHeight){
+        myBouncer.setY(myBouncer.getY() - myVelocityY * 0.5 * BOUNCER_SPEED * elapsedTime);
         if(myBouncer.getX()<0){
             myVelocityX *= -1;
         }
         if(myBouncer.getY()<0){
             myVelocityY *=-1;
         }
-        if(myBouncer.getX()>screenHeight){
+        if(myBouncer.getX()>screenHeight-myBouncer.getImage().getHeight()){
             myVelocityX *=-1;
         }
-        if(myBouncer.getY()>screenHeight){
-            move(0);
+        if(myBouncer.getY()>screenWidth-myBouncer.getImage().getWidth()){
+            myBouncer.setX(screenWidth / 2 - myBouncer.getBoundsInLocal().getWidth() / 2);
+            myBouncer.setY(screenHeight / 2 - myBouncer.getBoundsInLocal().getHeight() / 2);
+            numLives--;
+        }
+        if(numLives == 0){
+            //game over
         }
     }
-    public void reset(){
-
+    //will check for intersections with blocks rather than rectangles
+    //similar method will be created for paddle
+    public void checkIntersect(Rectangle shape, Group group){
+        if(myBouncer.intersects(shape.getLayoutBounds())){
+            group.getChildren().remove(shape);
+        }
+        //code to change direction of ball
     }
 }
